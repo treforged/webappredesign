57769b23746f3ee450921ba104ce426bc0b840ab
TRE Forged <tre@treforged.com>
2026-04-19 16:01:43 -0400

    src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App'
 
-const App = () => {
-  return (
-    <div style={{
-      color: 'white',
-      background: 'black',
-      height: '100vh',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      fontSize: '20px'
-    }}>
-      IOS RUNTIME TEST SUCCESS
-    </div>
-  )
-}
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
